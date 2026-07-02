487df400e0b3ac90e40a8b953a135420df17ec1a
Gabriel100-Pro <cortesempreender2.0@gmail.com>
2026-07-01 22:28:13 -0300

    Inicio da estilização do novo layout da 65Tech

---
+++ site-65Tech/assets/styles/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``
+
+export const TopBackGrond = styled.div``
+
+export const Title = styled.h1``
+
+export const ContainerInputs = styled.div``
+
+export const Input = styled.input``
+
+export const InputLabel = styled.label``
+
+export const Button = styled.button``
+
+export const Form = styled.form``
